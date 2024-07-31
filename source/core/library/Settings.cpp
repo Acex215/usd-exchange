86e78b59a9a49a741ce8b7476e9e7c6ccf88a99a
Andrew Kaufman <akaufman@nvidia.com>
2024-07-30 19:40:29 -0700

    Settings : Workaround Windows optimization issue

--- source/core/library/Settings.cpp
+++ source/core/library/Settings.cpp
@@ -22,3 +22,23 @@ TF_DEFINE_ENV_SETTING(
 );
 
 } // namespace usdex::core
+
+namespace
+{
+
+// This is a workaround for an optimization issue on Windows. If the TfEnvSettings are unused in the compilation unit in which
+// they are defined, they may be compiled out of the resulting object file. We did not exhaustively test all optimization settings,
+// but this issue did not occur on a Windows Debug build with optimization disabled. It also has not been seen on Linux, neither in
+// release nor debug builds, though only a limited compiler set was tested (gcc 7 and gcc 11). In any case, we can force the symbols
+// to remain if we use them directly. Each setting above must be used explicitly in the LoadSettings constructor below.
+struct LoadSettings
+{
+    LoadSettings()
+    {
+        TfGetEnvSetting(usdex::core::USDEX_ENABLE_OMNI_TRANSCODING);
+    }
+};
+
+static LoadSettings g_settings;
+
+} // namespace
