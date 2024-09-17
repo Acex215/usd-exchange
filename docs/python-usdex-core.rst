2de7e9a8b803cfefae7d1ac18a50b2086f2f77e5
Andrew Kaufman <akaufman@nvidia.com>
2024-09-17 13:01:03 -0700

    Docs : Add python module summaries

--- docs/python-usdex-core.rst
+++ docs/python-usdex-core.rst
@@ -2,6 +2,7 @@ usdex.core
 ----------
 
 .. automodule:: usdex.core
+    :autosummary:
     :platform: Windows-x86_64, Linux-x86_64
     :members:
     :imported-members:
--- source/test/python/__init__.py
+++ source/test/python/__init__.py
@@ -8,8 +8,8 @@
 # without an express license agreement from NVIDIA CORPORATION or
 # its affiliates is strictly prohibited.
 """
-`usdex.test <https://docs.omniverse.nvidia.com/usd/code-docs/usd-exchange-sdk/latest/docs/python-usdex-test.html>`_ provides ``unittest`` based test utilities
-for validating in-memory `OpenUSD <https://openusd.org/release/index.html>`_ data for consistency and correctness.
+`usdex.test <https://docs.omniverse.nvidia.com/usd/code-docs/usd-exchange-sdk/latest/docs/python-usdex-test.html>`_ provides ``unittest`` based test
+utilities for validating in-memory `OpenUSD <https://openusd.org/release/index.html>`_ data for consistency and correctness.
 """
 
 __all__ = [
