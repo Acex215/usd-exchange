d7cf46678aa56d9767d7fcb5ba1c4820f527767c
Andrew Kaufman <akaufman@nvidia.com>
2025-05-29 14:00:19 -0700

    Build : Leave plugInfo.json alone on Windows
    auditwheel only runs on Linux builds, and on Windows hashes are never injected into lib names. The normal ../../libusd_tf.so path is correct

--- tools/repoman/py_package.py
+++ tools/repoman/py_package.py
@@ -67,19 +67,21 @@ def setup_repo_tool(parser: argparse.ArgumentParser, config: Dict) -> Callable:
         with open(readme_target, "w") as f:
             f.writelines(data[4:7])
 
-        # All plugInfo LibraryPath values are going to be incorrect, because auditwheel appends hashes to lib names
-        # Fortunately, auditwheel also bakes rpaths into each module & the per plugin LibraryPath is unnecessary
-        # Rather than produce plugInfo with false data, we can set empty string to indicate the path is not used (matching usd monolithic approach)
-        for plugInfo in glob.glob(f"{stagingDir}/usd_exchange.libs/usd/*/resources/plugInfo.json"):
-            with open(plugInfo, "r") as f:
-                # remove illegal python style comments from json
-                plugContents = "".join([x for x in f.readlines() if not x.startswith("#")])
-            plugData = json.loads(plugContents)
-            for plug in plugData.get("Plugins", []):
-                if "LibraryPath" in plug:
-                    plug["LibraryPath"] = ""
-            with open(plugInfo, "w") as f:
-                json.dump(plugData, f, indent=4)
+        if omni.repo.man.is_linux():
+            # All plugInfo LibraryPath values are going to be incorrect, because auditwheel appends hashes to lib names
+            # Fortunately, auditwheel also bakes rpaths into each module & the per plugin LibraryPath is unnecessary
+            # Rather than produce plugInfo with false data, we can set empty string to indicate the path is not used.
+            # This matches OpenUSD's approach for usd monolithic builds.
+            for plugInfo in glob.glob(f"{stagingDir}/usd_exchange.libs/usd/*/resources/plugInfo.json"):
+                with open(plugInfo, "r") as f:
+                    # remove illegal python style comments from json
+                    plugContents = "".join([x for x in f.readlines() if not x.startswith("#")])
+                plugData = json.loads(plugContents)
+                for plug in plugData.get("Plugins", []):
+                    if "LibraryPath" in plug:
+                        plug["LibraryPath"] = ""
+                with open(plugInfo, "w") as f:
+                    json.dump(plugData, f, indent=4)
 
         # copy the pyproject setup script
         shutil.copyfile(omni.repo.man.resolve_tokens("$root/tools/pyproject/pybuild.py"), f"{stagingDir}/pybuild.py")
