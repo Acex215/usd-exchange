6b91371aa175a4df0f7c6387b3e02e5cb731381e
Andrew Kaufman <akaufman@nvidia.com>
2025-05-29 13:49:08 -0700

    Tests : Assert pxr modules can import without usdex.core
    This test currently passes in the main suite and fails in the whl suite on Windows, because the plugInfo.json LibraryPaths are empty, and rpath only fixes this on Linux

---
+++ source/core/tests/unittest/testPxr.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import subprocess
+import sys
+import unittest
+
+
+class PxrTest(unittest.TestCase):
+
+    def testPxrImport(self):
+        # Run in subprocess to avoid usdex.core already being imported
+        code = "from pxr import Tf; assert hasattr(Tf, 'Status')"
+        result = subprocess.run([sys.executable, "-c", code], capture_output=True)
+        self.assertEqual(result.returncode, 0, f"Failed to import pxr: {result.stderr.decode()}")
