613465a06cfb21385a28c23132d85435c50185cb
Andrew Kaufman <akaufman@nvidia.com>
2025-07-25 16:29:25 -0700

    Build : Add usdex.test to wheels
    Add optional dependency on omni.asset_validator wheel

--- tools/pyproject/pytest.sh
+++ tools/pyproject/pytest.sh
@@ -10,17 +10,11 @@ if [ -d $VENV ]; then
 fi
 ./_build/target-deps/python/python3 -m venv "$VENV"
 source "$VENV/bin/activate"
-python -m pip install _build/packages/*.whl
 
-# copy the test dependencies into the env
-TEST_ROOT=$1
-echo "Copy test deps from: ${TEST_ROOT}"
-SITE_PACKAGES=$(python -c "import site; print(site.getsitepackages()[-1])")
-echo "Copy test deps to: ${SITE_PACKAGES}"
-cp -r ${TEST_ROOT}/python/usdex/test ${SITE_PACKAGES}/usdex/test
-mkdir ${SITE_PACKAGES}/omni
-cp -r ${TEST_ROOT}/python/omni/asset_validator ${SITE_PACKAGES}/omni/asset_validator
-cp -r ${TEST_ROOT}/python/omni/capabilities ${SITE_PACKAGES}/omni/capabilities
+# Install packages with optional private index
+for wheel in _build/packages/*.whl; do
+    python -m pip install "${wheel}[test]"
+done
 
 # Run the tests
 python -m unittest discover -v -s source/core/tests/unittest
